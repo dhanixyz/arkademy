39a1f84c590c404ff9f4c83484a9afa4938319ab
dhanixyz <53263707+dhanixyz@users.noreply.github.com>
2020-10-16 15:19:26 +0700

    Update 1.php

--- 1.php
+++ 1.php
@@ -4,7 +4,7 @@ function data($name,$age) {
 	$data = array(
 		"name" => $name, 
 		"age" => $age,
-		"hobbies" => array("Ngoding","Main Musik","Videografi"),
+		"hobbies" => array("Ngoding","Tidur","Videografi"),
 		"is_married" => false,
 		"skills" => array(0 => array("skill_name" => "PHP", "level" => "beginner"), 1 => array("skill_name" => "NodeJS", "level" => "beginner")),
 		"interest_in_coding" => true
